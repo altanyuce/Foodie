c1b58c15778ef774355bedb4eec1aef748d45243
altanyuce <altanyuce160@gmail.com>
2025-10-16 23:48:39 +0300

    Initial commit - Foodie portfolio version

--- android_old/app/src/main/kotlin/com/foodie/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.foodie.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android_old/app/src/main/kotlin/com/foodie/foodie/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.foodie.foodie
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
